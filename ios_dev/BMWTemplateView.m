f6bc6e83c17acf40808c08cefd15e3b1212a4c4f
Rowan Chakoumakos <rowanc@stanford.edu>
2013-03-01 00:22:07 -0800

    @trusheim here's the first step, just needs to actually get the data

---
+++ ios_dev/BMWTemplateView.m
@@ -0,0 +1,75 @@
+//
+//  BMWGlympseVC.m
+//  TemplateName
+//
+//  Created by John Jessen on 6/5/12.
+//  Copyright (c) 2012 BMW Group. All rights reserved.
+//
+
+#import "BMWTemplateView.h"
+#import "BMWViewProvider.h"
+#import <BMWAppKit/BMWAppKit.h>
+
+@implementation BMWTemplateView
+
+/* Callback executed
+ * Only thing that is left to do is figure out how to get the value
+ */
+- (void)drivingSpeed:(NSDictionary *)test
+{
+    NSLog(@"TRYING TO OUTPUT DRIVING SPEED");
+    NSLog(CDSDrivingSpeedActual);
+    NSLog([test objectForKey:CDSDrivingSpeedActual]);
+    NSLog(@"nothing in the dict");
+}
+
+- (void)viewWillLoad:(IDView *)view
+{
+    
+    /* How to setup the callback */
+    IDCdsService *cdsService = [self.application cdsService];
+    [cdsService bindProperty:@"CDSDrivingSpeedActual" interval: 1 target:self selector:@selector(drivingSpeed:) completionBlock:^(NSError *error) {
+        NSLog(@"ERROR ERROR ERROR");
+        NSLog([error debugDescription]);
+    }];
+    /* END HOW TO SETUP THE CALLBACK */
+    
+    self.title = @"Template Title";
+    
+    IDLabel *label = [IDLabel label];
+    label.text = @"Template Label";
+    
+    IDButton *button1 = [IDButton button];
+    button1.text = @"Template Button";
+    button1.imageData = [self.application.imageBundle imageWithName:@"bmwLogo" type:@"png"];
+    [button1 setTarget:self selector:@selector(buttonPressed) forActionEvent:IDActionEventFocus];
+    
+    //    [self.application.imageBundle imageWithName:@"otherImage" type:@"png"];
+    
+    IDImage *image = [IDImage image];
+    image.imageData = [self.application.imageBundle imageWithData:[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"bmw" ofType:@"jpg"]]];
+    image.position = CGPointMake(100, 150);
+    
+    self.widgets = [NSArray arrayWithObjects:
+                    label,
+                    button1,
+                    image,
+                    nil];
+}
+
+static int counter = 0;
+
+- (void)buttonPressed
+{
+    if (counter % 2 == 0) {
+        [[self.widgets lastObject] setImageData:[self.application.imageBundle imageWithData:[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"scrat-bmw" ofType:@"jpg"]]]];
+    } else {
+        [[self.widgets lastObject] setImageData:[self.application.imageBundle imageWithName:@"bmw" type:@"jpg"]];
+        
+    }
+    
+    
+    counter++;
+}
+
+@end
