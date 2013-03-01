51c851653db346e25a4ac8b05884f6e39be8378f
brycecr <brycecr@gmail.com>
2013-03-01 00:20:07 -0800

    working example

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,6 +1,7 @@
+var chart;
 d3.json('data/cumulativeLineData.json', function (data) {
 	nv.addGraph(function() {
-		var chart = nv.models.cumulativeLineChart()
+		chart = nv.models.cumulativeLineChart()
 		    .x(function(d) { return d[0] })
 		    .y(function(d) { return d[1]/100 }) //adjusting, 100% is 1.00, not 100 as it is in the data
 		    .color(d3.scale.category10().range());
