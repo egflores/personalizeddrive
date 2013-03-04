0566287e5a15a9ab49d70b4a7de40ec0cf895d13
Vikas Yendluri <vikasuy@gmail.com>
2013-03-04 00:02:06 -0800

    woohoo! database -> frontend chart

--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
 import json
 from flask import Flask, render_template 
-from flask_peewee.db import Database
+# from flask_peewee.db import Database
+from peewee import Model, DateTimeField, IntegerField, MySQLDatabase
 
+"""
 DATABASE = {
     'engine': 'peewee.MySQLDatabase',
     'name': 'pd_test',
@@ -9,23 +11,39 @@ DATABASE = {
     'passwd': 'Nuggets',
     'host': 'bmw.stanford.edu'
 }
+db = Database(app)
+"""
 DEBUG = True 
 SECRET_KEY = 'StephenTrusheim'
 
 app = Flask(__name__) 
 app.config.from_object(__name__) 
-db = Database(app)
+db = MySQLDatabase('pd_test', user='pd_tester', passwd='Nuggets', host='bmw.stanford.edu')
+db.connect()
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class CarData(BaseModel):
+    time = DateTimeField()
+    mileage = IntegerField()
+
+def create_tables():
+    CarData.create_table()
 
 @app.route('/')
 def home():
+    values = []
+    for row in CarData.select():
+# values.append([row.time.isoformat(), row.mileage])
+        values.append([int(row.time.strftime("%s")), row.mileage])
+
     sample_data = {
         'data': [
             {
-                'key': 'MPG',
-                'values': [
-                    [0, 100],
-                    [100, 200]
-                ]
+                'key': 'Mileage',
+                'values': values
             }
         ]
     }
