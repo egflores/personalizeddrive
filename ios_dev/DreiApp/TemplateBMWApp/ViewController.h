27d65cc3449a14e2af9c28d2b5c097f8081373a2
Rowan Chakoumakos <rowanc@stanford.edu>
2013-03-03 21:31:43 -0800

    Initial Project Setup to test app

---
+++ ios_dev/DreiApp/TemplateBMWApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TemplateBMWApp
+//
+//  Created by John Jessen on 7/30/12.
+//  Copyright (c) 2012 BMW Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ios_dev/DreiApp/TemplateBMWApp/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  TemplateBMWApp
+//
+//  Created by John Jessen on 7/30/12.
+//  Copyright (c) 2012 BMW Group. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
