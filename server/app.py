42dc27f51fac6e7ecaf6c2a4ae0fb67055e98dac
Vikas Yendluri <vikasuy@gmail.com>
2013-03-03 23:30:23 -0800

    integrates bryce's charting code with python dict -> json object on client side

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template 
 from flask_peewee.db import Database
 
 DATABASE = {
@@ -8,16 +9,29 @@ DATABASE = {
     'passwd': 'Nuggets',
     'host': 'bmw.stanford.edu'
 }
-DEBUG = True
+DEBUG = True 
 SECRET_KEY = 'StephenTrusheim'
 
-app = Flask(__name__)
-app.config.from_object(__name__)
+app = Flask(__name__) 
+app.config.from_object(__name__) 
 db = Database(app)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def home():
+    sample_data = {
+        'data': [
+            {
+                'key': 'MPG',
+                'values': [
+                    [0, 100],
+                    [100, 200]
+                ]
+            }
+        ]
+    }
+    car_data = json.dumps(sample_data)
+    return render_template('index.html', car_data=car_data)
 
 if __name__ == '__main__':
     app.run()
+
