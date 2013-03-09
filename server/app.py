a3cd91c611651d20d8c36d6fd803b272e0f530e0
Vikas Yendluri <vikasuy@gmail.com>
2013-03-09 15:50:45 -0800

    new models! woooo

--- server/app.py
+++ server/app.py
@@ -1,9 +1,8 @@
 import json
 from flask import Flask, render_template 
-# from flask_peewee.db import Database
-from peewee import Model, DateTimeField, IntegerField, MySQLDatabase
+from flask_peewee.db import Database
+from peewee import DateTimeField, IntegerField, ForeignKeyField, DoubleField, CharField, TextField
 
-"""
 DATABASE = {
     'engine': 'peewee.MySQLDatabase',
     'name': 'pd_test',
@@ -11,32 +10,65 @@ DATABASE = {
     'passwd': 'Nuggets',
     'host': 'bmw.stanford.edu'
 }
-db = Database(app)
-"""
 DEBUG = True 
 SECRET_KEY = 'StephenTrusheim'
 
 app = Flask(__name__) 
 app.config.from_object(__name__) 
-db = MySQLDatabase('pd_test', user='pd_tester', passwd='Nuggets', host='bmw.stanford.edu')
-db.connect()
+db = Database(app)
 
-class BaseModel(Model):
-    class Meta:
-        database = db
-
-class CarData(BaseModel):
+class CarData(db.Model):
+    """ Just a Sample Model For Testing"""
     time = DateTimeField()
     mileage = IntegerField()
 
-def create_tables():
-    CarData.create_table()
+class User(db.Model):
+    first_name = CharField()
+    last_name = CharField()
+    password = TextField()
+
+class Car(db.Model):
+    user = ForeignKeyField(User, related_name="cars", index=True)
+    model = CharField()
+    year = IntegerField()
+    name = CharField()
+    picture = TextField()
+
+class Commute(db.Model):
+    car = ForeignKeyField(Car, related_name="commutes", index=True)
+    trip_mpg = DoubleField()
+    trip_mileage = DoubleField()
+    duration = IntegerField()
+
+class CBSMessage(db.Model):
+    car = ForeignKeyField(Car, related_name="cbs_messages", index=True)
+    type = CharField()
+    state = CharField()
+    description = TextField()
+    remaining_mileage = IntegerField(null=True)
+    due_date = DateTimeField(null=True)
+    update_time = DateTimeField(index=True)
+
+class CCMMessage(db.Model):
+    car = ForeignKeyField(Car, related_name="ccm_messages", index=True)
+    ccm_id = IntegerField()
+    mileage = IntegerField()
+    description = TextField()
+    update_time = DateTimeField(index=True)
+
+class RawData(db.Model):
+    car = ForeignKeyField(Car, related_name="raw_data", index=True)
+    remaining_fuel = IntegerField() # in liters
+    remaining_range_fuel = IntegerField()
+    range_unit = CharField()
+    mileage = IntegerField()
+    ave_mpg = DoubleField()
+    update_time = DateTimeField(index=True) 
 
 @app.route('/')
 def home():
     values = []
     for row in CarData.select():
-# values.append([row.time.isoformat(), row.mileage])
         values.append([int(row.time.strftime("%s")), row.mileage])
 
     sample_data = {
@@ -51,5 +83,12 @@ def home():
     return render_template('index.html', car_data=car_data)
 
 if __name__ == '__main__':
+    User.create_table(fail_silently=True)
+    Car.create_table(fail_silently=True)
+    Commute.create_table(fail_silently=True)
+    CBSMessage.create_table(fail_silently=True)
+    CCMMessage.create_table(fail_silently=True)
+    RawData.create_table(fail_silently=True)
+
     app.run()
 
