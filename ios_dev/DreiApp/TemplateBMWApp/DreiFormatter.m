5452ecba1e8a68f369e5ed27e1264221282f7b25
Rowan Chakoumakos <rowanc@stanford.edu>
2013-03-10 16:15:53 -0700

    Added data formmating that takes BMW specific api data and formats it to our general API so that this is the only class that would need to be changed on each iPhone for at least the sending side of things

---
+++ ios_dev/DreiApp/TemplateBMWApp/DreiFormatter.m
@@ -0,0 +1,13 @@
+//
+//  DreiFormatter.m
+//  TemplateBMWApp
+//
+//  Created by Rowan Chakoumakos on 3/10/13.
+//  Copyright (c) 2013 BMW Group. All rights reserved.
+//
+
+#import "DreiFormatter.h"
+
+@implementation DreiFormatter
+
+@end
