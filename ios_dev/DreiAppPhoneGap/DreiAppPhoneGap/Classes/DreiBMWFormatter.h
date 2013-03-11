6da88d8db179e583aed18771c7acd08d50255610
trusheim <strusheim@gmail.com>
2013-03-10 23:45:55 -0700

    Updating formatter to handle individual entries

--- ios_dev/DreiAppPhoneGap/DreiAppPhoneGap/Classes/DreiBMWFormatter.h
+++ ios_dev/DreiAppPhoneGap/DreiAppPhoneGap/Classes/DreiBMWFormatter.h
@@ -11,4 +11,7 @@
 
 @interface DreiBMWFormatter : NSObject <DreiFormatter>
 +(NSData *)formatCarData:(NSArray *)carData error:(NSError *)error;
++(NSData *)formatDataEntry:(NSDictionary *)originalDataPoint error:(NSError *)error;
++(NSMutableDictionary *)convertDataEntry:(NSDictionary *)originalDataPoint error:(NSError *)error;
+    
 @end
--- ios_dev/DreiAppPhoneGap/DreiAppPhoneGap/Classes/DreiBMWFormatter.m
+++ ios_dev/DreiAppPhoneGap/DreiAppPhoneGap/Classes/DreiBMWFormatter.m
@@ -22,59 +22,71 @@
 +(NSData *)formatCarData:(NSArray *)carData error:(NSError *)error; {
     NSMutableArray * formattedCarData = [[NSMutableArray alloc] initWithCapacity:[carData count]];
     for (NSDictionary * originalDataPoint in carData) {
-        NSMutableDictionary * formattedDataPoint = [[NSMutableDictionary alloc] initWithCapacity:[originalDataPoint count]];
-        formattedDataPoint[@"car_id"] = [NSNumber numberWithInt:1]; //TODO: Remove hardcoded number.  Blocking on accessing VIN number.
-        
-        if(originalDataPoint[@"headlights"] == 0) {
-            formattedDataPoint[@"headlights"] = @"yes";
-        } else {
-            formattedDataPoint[@"headlights"] = @"no";
-        }
-        
-        formattedDataPoint[@"odometer"] = originalDataPoint[@"odometer"];
-        formattedDataPoint[@"speed"] = originalDataPoint[@"speedActual"];
-        switch ((int)originalDataPoint[@"engine_status"]) {
-            case CDSEngineStatus_OFF:
-                formattedDataPoint[@"engine_status"] = @"off";
-                break;
-                
-            case CDSEngineStatus_STARTING:
-                formattedDataPoint[@"engine_status"] = @"starting";
-                break;
-            
-            case CDSEngineStatus_RUNNING:
-                formattedDataPoint[@"engine_status"] = @"running";
-                break;
-                
-            case CDSEngineStatus_INVALID:
-                formattedDataPoint[@"engine_status"] = @"invalid";
-                break;
-        }
-        //formattedDataPoint["navigation_destinations"]; TODO: Version 1.01...if we want them
-        formattedDataPoint[@"fuel_level"] = originalDataPoint[@"fuel_level"];
-        formattedDataPoint[@"fuel_range"] = originalDataPoint[@"fuel_range"];
-        
-        
-        switch ((int)originalDataPoint[@"fuel_reserve"]) {
-            case CDSSensorsFuelReserve_NO:
-                formattedDataPoint[@"fuel_reserve"] = @"no";
-                break;
-                
-            case CDSSensorsFuelReserve_YES:
-                formattedDataPoint[@"fuel_reserve"] = @"yes";
-                break;
-                
-            case CDSSensorsFuelReserve_INVALID:
-                formattedDataPoint[@"fuel_reserve"] = @"invalid";
-                break;
-        }
-
-        formattedDataPoint[@"timestamp"] = originalDataPoint[@"time"];
+        NSMutableDictionary * formattedDataPoint = [DreiBMWFormatter convertDataEntry:originalDataPoint error:error];
         [formattedCarData addObject:formattedDataPoint];
         NSLog(@"%@", formattedCarData);
     }
     NSDictionary * dataToSend = [NSDictionary dictionaryWithObject:formattedCarData forKey:@"data"];
     return [NSJSONSerialization dataWithJSONObject:dataToSend
+                                           options:NSJSONWritingPrettyPrinted
+                                             error:&error];
+}
+
++(NSMutableDictionary *)convertDataEntry:(NSDictionary *)originalDataPoint error:(NSError *)error {
+    NSMutableDictionary * formattedDataPoint = [[NSMutableDictionary alloc] initWithCapacity:[originalDataPoint count]];
+    formattedDataPoint[@"car_id"] = [NSNumber numberWithInt:1]; //TODO: Remove hardcoded number.  Blocking on accessing VIN number.
+    
+    if(originalDataPoint[@"headlights"] == 0) {
+        formattedDataPoint[@"headlights"] = @"yes";
+    } else {
+        formattedDataPoint[@"headlights"] = @"no";
+    }
+    
+    formattedDataPoint[@"odometer"] = originalDataPoint[@"odometer"];
+    formattedDataPoint[@"speed"] = originalDataPoint[@"speedActual"];
+    switch ((int)originalDataPoint[@"engine_status"]) {
+        case CDSEngineStatus_OFF:
+            formattedDataPoint[@"engine_status"] = @"off";
+            break;
+            
+        case CDSEngineStatus_STARTING:
+            formattedDataPoint[@"engine_status"] = @"starting";
+            break;
+            
+        case CDSEngineStatus_RUNNING:
+            formattedDataPoint[@"engine_status"] = @"running";
+            break;
+            
+        case CDSEngineStatus_INVALID:
+            formattedDataPoint[@"engine_status"] = @"invalid";
+            break;
+    }
+    //formattedDataPoint["navigation_destinations"]; TODO: Version 1.01...if we want them
+    formattedDataPoint[@"fuel_level"] = originalDataPoint[@"fuel_level"];
+    formattedDataPoint[@"fuel_range"] = originalDataPoint[@"fuel_range"];
+    
+    
+    switch ((int)originalDataPoint[@"fuel_reserve"]) {
+        case CDSSensorsFuelReserve_NO:
+            formattedDataPoint[@"fuel_reserve"] = @"no";
+            break;
+            
+        case CDSSensorsFuelReserve_YES:
+            formattedDataPoint[@"fuel_reserve"] = @"yes";
+            break;
+            
+        case CDSSensorsFuelReserve_INVALID:
+            formattedDataPoint[@"fuel_reserve"] = @"invalid";
+            break;
+    }
+    
+    formattedDataPoint[@"timestamp"] = originalDataPoint[@"time"];
+    return formattedDataPoint;
+}
+
++(NSData *)formatDataEntry:(NSDictionary *)originalDataPoint error:(NSError *)error {
+    NSMutableDictionary * formattedDataPoint = [DreiBMWFormatter convertDataEntry:originalDataPoint error:error];
+    return [NSJSONSerialization dataWithJSONObject:formattedDataPoint
                                            options:NSJSONWritingPrettyPrinted
                                              error:&error];
 }
