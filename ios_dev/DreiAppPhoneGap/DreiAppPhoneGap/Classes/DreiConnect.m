477c52d38afb8c1c3bc2d18f60a458a9c3b6f4e9
trusheim <strusheim@gmail.com>
2013-03-10 21:44:42 -0700

    Working PhoneGap integration

--- ios_dev/DreiAppPhoneGap/DreiAppPhoneGap/Classes/DreiConnect.m
+++ ios_dev/DreiAppPhoneGap/DreiAppPhoneGap/Classes/DreiConnect.m
@@ -14,14 +14,10 @@
 - (void)start:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
     
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
+
 @end
