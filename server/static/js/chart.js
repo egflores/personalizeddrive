8df2c0a152c2376b6174ce3823b1266d35041b45
brycecr <brycecr@gmail.com>
2013-03-10 18:15:52 -0700

    rolled me own silly gagues

--- server/static/js/chart.js
+++ server/static/js/chart.js
@@ -1,5 +1,6 @@
 var chart;
 var smallchart;
+var donut;
 data = car_data.data;
 nv.addGraph(function() {
     chart = nv.models.lineChart()
@@ -42,9 +43,53 @@ nv.addGraph(function() {
 
 	d3.select(".container").select('#smallchart svg').selectAll("text").style("font-size","10px");
 
-	
-
 	nv.utils.windowResize(chart.update);
 
 	return chart;
+});
+
+function exampleData() {
+  return [
+  {
+    key: "data",
+    values: [
+      { 
+        "label" : "" ,
+        "value" : 30
+      } , 
+      { 
+        "label" : "Fuel" , 
+        "value" : 70
+      }
+    ]
+  }
+  ];
+}
+
+nv.addGraph(function() {
+   var donut = nv.models.pieChart()
+       .x(function(d) { return d.label })
+       .y(function(d) { return d.value })
+       .showLabels(false)
+	  .showLegend(false)
+       .donut(true);
+
+	data = exampleData();
+ 
+     d3.select("#donut svg")
+         .datum(data)
+         .transition().duration(1200)
+         .call(donut);
+
+	d3.select("#donut svg").append('text')
+		.text(data[0].values[1].value.toString())
+		.attr('x', 107)
+		.attr('y', 180)
+		.attr('fill', "rgb(58, 135, 173)")
+		.style("font-family", "\"Helvetica Neue\",Helvetica,Arial,sans-serif")
+		.style("font-weight", "bold")
+		.style('font-size', "80px")
+		.style('text-shadow', "0px 4px 0px rgba(88, 88, 88, 0.5)");
+ 
+	return donut;
 });
