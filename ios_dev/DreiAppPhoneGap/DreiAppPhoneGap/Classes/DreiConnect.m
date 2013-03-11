475ca6bd87ff2b27ffa321fe9a87a6e1857adcaa
Rowan Chakoumakos <rowanc@stanford.edu>
2013-03-11 03:45:16 -0700

    Fixed formatting so the javascript could actually be written correctly

--- ios_dev/DreiAppPhoneGap/DreiAppPhoneGap/Classes/DreiConnect.m
+++ ios_dev/DreiAppPhoneGap/DreiAppPhoneGap/Classes/DreiConnect.m
@@ -47,7 +47,7 @@
  * TODO: Rowan, Javascript master, needs to look at this.
  */
 -(void) sendMessage:(NSString *)message toCallback:(NSString *)callback {
-    NSString *newString = [NSString stringWithFormat:@"drei_callback_%@(\"%@\")",callback,message];
+    NSString *newString = [NSString stringWithFormat:@"drei_callback_%@(%@);",callback,message];
     [self writeJavascript:newString];
     NSLog(newString);
     //CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
