64c7e6fb70784400a90f9351e4824065bfe0af26
brycecr <brycecr@gmail.com>
2013-03-10 18:23:43 -0700

    prettifying gague code

--- server/static/js/chart.js
+++ server/static/js/chart.js
@@ -48,48 +48,56 @@ nv.addGraph(function() {
 	return chart;
 });
 
+
+/*
+ TO BE DELETED WHEN RENDERED OBSOLETE
+*/
 function exampleData() {
   return [
   {
     key: "data",
     values: [
       { 
-        "label" : "" ,
-        "value" : 30
+        "label" : "Fuel" ,
+        "value" : 70
       } , 
       { 
-        "label" : "Fuel" , 
-        "value" : 70
+        "label" : "" , 
+        "value" : 30
       }
     ]
   }
   ];
 }
 
-nv.addGraph(function() {
-   var donut = nv.models.pieChart()
-       .x(function(d) { return d.label })
-       .y(function(d) { return d.value })
-       .showLabels(false)
-	  .showLegend(false)
-       .donut(true);
 
-	data = exampleData();
- 
-     d3.select("#donut svg")
-         .datum(data)
-         .transition().duration(1200)
-         .call(donut);
+function makegauge(data, id) {
+	nv.addGraph(function() {
+	   var donut = nv.models.pieChart()
+		  .x(function(d) { return d.label })
+		  .y(function(d) { return d.value })
+		  .showLabels(false)
+		  .showLegend(false)
+		  .donut(true);
 
-	d3.select("#donut svg").append('text')
-		.text(data[0].values[1].value.toString())
-		.attr('x', 107)
-		.attr('y', 180)
-		.attr('fill', "rgb(58, 135, 173)")
-		.style("font-family", "\"Helvetica Neue\",Helvetica,Arial,sans-serif")
-		.style("font-weight", "bold")
-		.style('font-size', "80px")
-		.style('text-shadow', "0px 4px 0px rgba(88, 88, 88, 0.5)");
- 
-	return donut;
-});
+		d3.select("#"+id+" svg")
+		    .datum(data)
+		    .transition().duration(1200)
+		    .call(donut);
+
+		d3.select("#"+id+" svg").append('text')
+			.text(data[0].values[0].value.toString())
+			.attr('x', 107)
+			.attr('y', 180)
+			.attr('fill', "rgb(58, 135, 173)")
+			.style("font-family", "\"Helvetica Neue\",Helvetica,Arial,sans-serif")
+			.style("font-weight", "bold")
+			.style('font-size', "80px")
+			.style('text-shadow', "0px 4px 0px rgba(88, 88, 88, 0.5)");
+	 
+		return donut;
+	});
+}
+
+makegauge(exampleData(), "donut");
+
