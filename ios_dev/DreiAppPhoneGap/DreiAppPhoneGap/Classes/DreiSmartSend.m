e4b39b2020fbf3c87d34e6b938b92f658e30995a
Rowan Chakoumakos <rowanc@stanford.edu>
2013-04-16 01:21:16 -0700

    Added in backoff on upload attempts after a successful upload

--- ios_dev/DreiAppPhoneGap/DreiAppPhoneGap/Classes/DreiSmartSend.m
+++ ios_dev/DreiAppPhoneGap/DreiAppPhoneGap/Classes/DreiSmartSend.m
@@ -10,26 +10,28 @@
 #import "DreiSmartSend.h"
 #import "Reachability.h"
 
+#define INTERVAL_INITIAL 1          //1 minute
+#define INTERVAL_AFTER_UPLOAD 1200 //20 minutes
+
 @implementation DreiSmartSend
 
 NSTimer * uploadTimer;
-NSTimeInterval timeBeforeNextAttempt;
 bool hasStarted = false;
 
 -(void)start {
-    NSLog(@"SMART SEND");
     if(hasStarted) return;
-    timeBeforeNextAttempt = 10;
-    uploadTimer = [NSTimer scheduledTimerWithTimeInterval:timeBeforeNextAttempt target:self selector:@selector(tryUpload) userInfo:nil repeats:YES];
+    uploadTimer = [NSTimer scheduledTimerWithTimeInterval:INTERVAL_INITIAL target:self selector:@selector(tryUpload) userInfo:nil repeats:YES];
 }
 
 -(void)tryUpload {
     NetworkStatus currentStatus = [[Reachability reachabilityForInternetConnection]
                                    currentReachabilityStatus];
     
+    bool shouldUpload = false;
     if(currentStatus == ReachableViaWWAN) { // 3G
         NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
         BOOL uploadOn3g = [defaults boolForKey:@"uploadOn3g"];
+        shouldUpload = uploadOn3g;
         if(uploadOn3g) {
             NSLog(@"3G - should upload");
         } else {
@@ -37,16 +39,24 @@ bool hasStarted = false;
         }
         
     } else if(currentStatus == ReachableViaWiFi) { //wifi
+        shouldUpload = true;
         NSLog(@"wifi - should upload");
     } else if(currentStatus == NotReachable) { // no connection currently possible
         NSLog(@"No connection");
     }
-    
+    if(shouldUpload) {
+        NSLog(@"Should upload the data");
+        //if upload success
+        //Set the timer interval to now be 20 minutes
+        [uploadTimer invalidate];
+        uploadTimer = [NSTimer scheduledTimerWithTimeInterval:INTERVAL_AFTER_UPLOAD target:self selector:@selector(tryUpload) userInfo:nil repeats:YES];
+        
+    }
 }
 
 -(void)stop {
     if(!hasStarted) return;
-    
+    [uploadTimer invalidate];
 }
 
 @end
