5d0823da16eaad9bea1f4d22d2a7f4dac793b215
trusheim <strusheim@gmail.com>
2013-04-29 01:35:05 -0700

    Working on code for OBD2 synchronous (timed) data collection. @rowanc and I need to test!

---
+++ ios_dev/OBD2Kit/SampleApps/BasicScan/Classes/DreiSynchDataProvider.h
@@ -0,0 +1,55 @@
+//
+//  DreiSyncDataProvider.h
+//  BasicScan
+//
+//  Created by Stephen Trusheim on 4/29/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+/* The DreiSynchDataProvider takes the asynchronous CDS API and makes it deliver consistent, predictable data at regular, time-stamped intervals. The main benefit / reason for this framework is two-fold: 1. make us able to have constant data logging over the API, and 2. to allow us to make fake data when we're simulating.
+ *
+ * The data service sits on top of the async calls and manages that interaction entirely. Just call startCollection, stopCollection, and getData when necessary.
+ *
+ * Calling getData will return an NSArray of NSDictionaries. Each dictionary represents a single time slice, and has all the keys defined in getDataKeys, and one more: "time". The data returned is all in the format it was in when it came off the BMW API. The data in each dictionary is the most recent data when that entry was logged. The data will come in at approximately the logging interval.
+ *
+ * If no data is available for a given piece of information (e.g. the speed never came in), the dictionary value for that item will be the String "NODATA".
+ *
+ * This class should not be instantiated directly. Use DreiSynchDataProvider_BMW (for when you're in production in a car) or DreiSynchDataProvider_Random (for on your laptop - the BMW simulator doesn't really let you make up fake data).
+ *
+ * TODOS: allow user to specify the logging interval, get data as it's streaming in (delegate function). Check memory management.
+ */
+
+
+@interface DreiSynchDataProvider : NSObject
+
+@property (atomic, retain) NSMutableArray * _dataStore;
+@property (atomic,retain) NSMutableDictionary * _currentValues;
+@property (atomic,retain) NSTimer * _synchTimer;
+@property (atomic) float interval;
+
+/* Sets all the data keys to "NODATA", the sentinel for when no data has come in. Call this to invalidate old data. */
+-(void)initData:(NSArray *)keys;
+
+/* Returns all the keys that will be in each returned NSDictionary. */
+-(NSArray *)getDataKeys;
+
+/* Starts data collection (logs to _dataStore) */
+-(void)startCollection;
+
+/* Stops data collection */
+-(void)stopCollection;
+
+/* Returns _dataStore - call this instead of using the value directly for forward compatibility. */
+-(NSArray *)getAllData;
+
+/* Deletes all data currently stored */
+-(void)clearStoredData;
+
+/* Returns the latest data that we got */
+-(NSDictionary *)getLatestData;
+
+/* internal - normalize data and compute computable values, remove values that shouldn't be there */
+- (NSMutableDictionary *) processDataPoint:(NSMutableDictionary *)data;
+@end
