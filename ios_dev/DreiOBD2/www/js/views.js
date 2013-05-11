69576f3769ed648aa84b98a540a0c663338fe024
Rowan Chakoumakos <rowanc@stanford.edu>
2013-05-10 23:33:57 -0700

    Added start of onboarding, and am working on improving your UI

--- ios_dev/DreiOBD2/www/js/views.js
+++ ios_dev/DreiOBD2/www/js/views.js
@@ -3,8 +3,12 @@ $().ns('DreiApp.Views');
 
 $(document).ready(function () {
 
+    DreiApp.Views.MainView = Backbone.View.extend({
+        transition: "fade"
+    });
 
-    DreiApp.Views.DriveHistory = Backbone.View.extend({
+
+    DreiApp.Views.DriveHistory = DreiApp.Views.MainView.extend({
 
         template:_.template($('#DriveHistoryTemplate').html()),
 
@@ -14,7 +18,7 @@ $(document).ready(function () {
         }
     });
 
-    DreiApp.Views.Track = Backbone.View.extend({
+    DreiApp.Views.Track = DreiApp.Views.MainView.extend({
 
         template:_.template($('#TrackTemplate').html()),
 
@@ -24,7 +28,31 @@ $(document).ready(function () {
         }
     });
 
-    DreiApp.Views.Settings = Backbone.View.extend({
+    DreiApp.Views.ActiveTracking = DreiApp.Views.MainView.extend({
+
+        transition: "slideup",
+
+        template:_.template($('#ActiveTrackingTemplate').html()),
+
+        render:function (eventName) {
+            $(this.el).html(this.template());
+            return this;
+        }
+    });
+
+    DreiApp.Views.Onboarding = DreiApp.Views.MainView.extend({
+
+        transition: "slideup",
+
+        template:_.template($('#OnboardingTemplate').html()),
+
+        render:function (eventName) {
+            $(this.el).html(this.template());
+            return this;
+        }
+    });
+
+    DreiApp.Views.Settings = DreiApp.Views.MainView.extend({
 
         template:_.template($('#SettingsTemplate').html()),
 
